6ba835677b84e90a5f0cd909ceab2aba520b9c33
Gaurav <gauravraj7011@gmail.com>
2025-02-12 15:06:10 +0530

    updates

--- main/script.js
+++ main/script.js
@@ -12,7 +12,8 @@ let isResponsesGenerating = false;
 
 //API configuration
 
-
+const GEMINI_API_KEY = "AIzaSyBnftETss3NNfBjo7Fu0t-c4jp4TyAy8ow";
+const API_URL= `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 function loadLocalstorageData() {
     const savedChats = localStorage.getItem("savedChats");
