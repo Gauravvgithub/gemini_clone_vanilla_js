74d67f7d4180aa4926cc6e5a8e75bdd49e5f4ca8
Gauravvgithub <gauravraj7011@gmail.com>
2025-06-22 22:25:31 +0530

    deployment

---
+++ config.js
@@ -0,0 +1,3 @@
+const CONFIG = {
+    GEMINI_API_KEY:"AIzaSyBnftETss3NNfBjo7Fu0t-c4jp4TyAy8ow"
+}
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let isResponsesGenerating = false;
 
 //API configuration
 
-const GEMINI_API_KEY = "AIzaSyBnftETss3NNfBjo7Fu0t-c4jp4TyAy8ow";
+const GEMINI_API_KEY = `${CONFIG.GEMINI_API_KEY}`;
 const API_URL= `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 function loadLocalstorageData() {
